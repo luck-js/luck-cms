8441394be45abfe62b0a2456703ac785e3cbbe8d
Daniel Karski <daniel.karski5q@gmail.com>
2019-09-19 16:46:25 +0200

    Handled polish marks

--- api/post/models/Post.js
+++ api/post/models/Post.js
@@ -3,8 +3,41 @@
 /**
  * Lifecycle callbacks for the `Post` model.
  */
-const getSlug = title =>
-  title
+
+const sets = [
+  {to: 'a', from: '[ÀÁÂÃÄÅÆĀĂĄẠẢẤẦẨẪẬẮẰẲẴẶἀ]'},
+  {to: 'c', from: '[ÇĆĈČ]'},
+  {to: 'd', from: '[ÐĎĐÞ]'},
+  {to: 'e', from: '[ÈÉÊËĒĔĖĘĚẸẺẼẾỀỂỄỆ]'},
+  {to: 'g', from: '[ĜĞĢǴ]'},
+  {to: 'h', from: '[ĤḦ]'},
+  {to: 'i', from: '[ÌÍÎÏĨĪĮİỈỊ]'},
+  {to: 'j', from: '[Ĵ]'},
+  {to: 'ij', from: '[Ĳ]'},
+  {to: 'k', from: '[Ķ]'},
+  {to: 'l', from: '[ĹĻĽŁ]'},
+  {to: 'm', from: '[Ḿ]'},
+  {to: 'n', from: '[ÑŃŅŇ]'},
+  {to: 'o', from: '[ÒÓÔÕÖØŌŎŐỌỎỐỒỔỖỘỚỜỞỠỢǪǬƠ]'},
+  {to: 'oe', from: '[Œ]'},
+  {to: 'p', from: '[ṕ]'},
+  {to: 'r', from: '[ŔŖŘ]'},
+  {to: 's', from: '[ßŚŜŞŠȘ]'},
+  {to: 't', from: '[ŢŤ]'},
+  {to: 'u', from: '[ÙÚÛÜŨŪŬŮŰŲỤỦỨỪỬỮỰƯ]'},
+  {to: 'w', from: '[ẂŴẀẄ]'},
+  {to: 'x', from: '[ẍ]'},
+  {to: 'y', from: '[ÝŶŸỲỴỶỸ]'},
+  {to: 'z', from: '[ŹŻŽ]'},
+  {to: '-', from: '[·/_,:;\']'}
+];
+
+const getSlug = title => {
+  sets.forEach(set => {
+    title = title.replace(new RegExp(set.from,'gi'), set.to)
+  });
+
+  return title
     .toString()
     .toLowerCase()
     .replace(/\s+/g, "-") // Replace spaces with -
@@ -13,6 +46,8 @@ const getSlug = title =>
     .replace(/--+/g, "-") // Replace multiple - with single -
     .replace(/^-+/, "") // Trim - from start of text
     .replace(/-+$/, "") // Trim - from end of text
+}
+
 module.exports = {
   // Before saving a value.
   // Fired before an `insert` or `update` query.
