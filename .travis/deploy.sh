602489dbe188c73558db98ae123a97e0409939cd
Daniel Karski <daniel.karski5q@gmail.com>
2019-09-20 18:37:22 +0200

    Handle image upload

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,7 +4,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker pull $CMS_IMAGE_NAME
 docker stop $CMS_CONTAINER_NAME || true && docker rm $CMS_CONTAINER_NAME || true
 docker run --env-file /root/$VIRTUAL_HOST/envs_db -v /root/$VIRTUAL_HOST/data-db:/data/db -d --name $DB_CONTAINER_NAME mongo:latest
-docker run --env-file /root/$VIRTUAL_HOST/envs_cms -e VIRTUAL_HOST=www.$VIRTUAL_HOST,$VIRTUAL_HOST -e LETSENCRYPT_HOST=$VIRTUAL_HOST -d --link $DB_CONTAINER_NAME:db --name $CMS_CONTAINER_NAME $CMS_IMAGE_NAME
+docker run --env-file /root/$VIRTUAL_HOST/envs_cms -e VIRTUAL_HOST=www.$VIRTUAL_HOST,$VIRTUAL_HOST -e LETSENCRYPT_HOST=$VIRTUAL_HOST -d --link $DB_CONTAINER_NAME:db -v /root/$VIRTUAL_HOST/strapi-app-uploads:/usr/src/api/strapi-app/public/uploads --name $CMS_CONTAINER_NAME $CMS_IMAGE_NAME
 if [ "$(docker ps -q -f name=nginx)" ]; then
   docker exec nginx nginx -s reload
 fi
